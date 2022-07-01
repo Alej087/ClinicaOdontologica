92c0f9cb2ae85a61c7ea688ae35d2fc8d93c5852
Alej087 <alejo87.gonzalez@gmail.com>
2022-07-01 13:24:10 -0500

    repository created

---
+++ src/main/java/com/proyectos/ClinicaOdontologica/repository/AddressRepository.java
@@ -0,0 +1,9 @@
+package com.proyectos.ClinicaOdontologica.repository;
+
+import com.dh.clase36.integradora.entities.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, Long> {
+}
---
+++ src/main/java/com/proyectos/ClinicaOdontologica/repository/AppointmentRepository.java
@@ -0,0 +1,9 @@
+package com.proyectos.ClinicaOdontologica.repository;
+
+import com.dh.clase36.integradora.entities.Appointment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+}
